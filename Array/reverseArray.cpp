de4eb61664c66949258cf560c4e00a24f535f10b
himanshu-tawra <himanshu.tawra01@gmail.com>
2020-06-13 21:44:01 +0530

    Updated reverseArray.cpp

--- Array/reverseArray.cpp
+++ Array/reverseArray.cpp
@@ -1,45 +1,44 @@
 #include<iostream>
 using namespace std;
- 
-void swap(int &x,int &y){
-    int temp = x;
-    x = y;
-    y = temp;
+
+void swap(int *x,int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
-void reverseArray(int *arr ,int len){
-
+int reverseArray(int * arr,int len){
     int i = 0;
-    int j = len-1;
+    int j = len - 1;
 
     while(i < j){
-        swap(arr[i] , arr[j]);
+        swap(arr[i],arr[j]);
         i++;
         j--;
     }
+
 }
 
-void printArray(int *arr, int len){
+void printArray(int *arr,int len){
     for(int i = 0; i < len; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
 
-
-
 int main(){
 
-    int arr[] = {1,2,3,4,5,6,7,8,9};
-    int len = sizeof(arr)/sizeof(arr[0]);
+    int arr[1000];
+    int size;
+    cin>>size;
 
-    cout<<"Original Array"<<endl;
-    printArray(arr,len);
+    for(int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
 
-    reverseArray(arr,len);
+    reverseArray(arr,size);
+    printArray(arr,size);
 
-    cout<<"Array after swapping"<<endl;
-    printArray(arr,len);
-    
+
     return 0;
 }
